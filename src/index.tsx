e8652d12f414be8bc4b69043b14808df578be00f
Kallu609 <khirvola@gmail.com>
2019-01-30 22:54:00 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
 render(<App />, document.getElementById('root'));
+registerServiceWorker();
