0395491c990935c087e915eefb9c666fc387277e
Kallu609 <khirvola@gmail.com>
2019-01-31 15:08:35 +0200

    update service worker
    upgrade packages and .gitignore
    
    make serviceWorker comply tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 render(<App />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.unregister();
